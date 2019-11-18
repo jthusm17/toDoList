2a1452e9c129a2cfe0dba4bcf6d9a6feff2205fe
jthusm17 <54552549+jthusm17@users.noreply.github.com>
2019-11-18 14:28:26 -0600

    basic setup w/ vue

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
 console.log("initialized");
+var app = new Vue({
+    el: '#app', 
+    data: {
+        listName: "To Do",
+        itemId: 0
+    },
+    methods: {
+        addItem: function(){
+            this.itemId += 1
+        }
+    }
+})
