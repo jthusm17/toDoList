1dd697bfe8163ae1218e6b85c23c0f7b13582994
jthusm17 <54552549+jthusm17@users.noreply.github.com>
2019-12-16 17:46:05 -0600

    better outline

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 console.log("initialized");
-
+/*
 Vue.component('row', {
     template: `
     <div>
     <input v-model="listDesc"> 
-    <button>Add</button>
+    <button @click="addItem"> Add </button>
     </div>
     `,
     
@@ -15,15 +15,51 @@ Vue.component('row', {
     }
     
 })
+*/
+Vue.component('item', {
+    props: {
+        list: {
+            type: String
+        }
+    },
+    template: `
+    <div>
+    <div>
+    <button @click="acompleteItem">complete</button>
+    <button>delete</button>
+    </div>
+    {{list}}
+    <div>
+    <p> - - - - - - -</p>
+    </div>
+    </div>
+    `,
+    
+    data() {
+        return { 
+            list: "o",
+            methods: {
+                completeItem: function(){
+                    list.strike()
+                }
+            }
+        }
+    }
+    
+})
+
 var app = new Vue({
     el: '#app', 
     data: {
+        list: "",
         listName: "To Do",
-        itemId: 0
+        x: 0,
+        itemId: []
     },
     methods: {
         addItem: function(){
-            this.itemId += 1
+            this.x += 1
+            this.itemId += this.x
         }
     }
 })
